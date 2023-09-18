68092a6e47936c6a448e82f2fe2fbcd559ede7a0
stoledcat <jetnavo@yandex.ru>
2023-09-18 21:24:14 +0300

    fix welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-import prompt
+from brain_games.cli import welcome_user
 
 
 def main():
-    global name
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
+    welcome_user()
 
 
 if __name__ == '__main__':
