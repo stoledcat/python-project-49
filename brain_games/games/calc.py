944c446b3dbaddc2ead765d780e34118011f9fb6
stoledcat <jetnavo@yandex.ru>
2023-09-23 17:03:22 +0300

    refactoring

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,26 +1,13 @@
 from operator import add, sub, mul
 from random import randint, choice
-import prompt
-from brain_games.cli import welcome_user
+
+operations = ((add, '+'), (sub, '-'), (mul, '*'))
+description = 'What is the result of the expression?'
 
 
-def calculation():
-    user_name = welcome_user()
-    print('What is the result of the expression?')
-    operations = ((add, '+'), (sub, '-'), (mul, '*'))
-    count = 0
-    for i in range(3):
-        number1, number2 = randint(1, 10), randint(1, 10)
-        operator, sign = choice(operations)
-        print(f'Question: {number1} {sign} {number2}')
-        user_answer = prompt.string('Your answer: ')
-        right_answer = str(operator(number1, number2))
-        if user_answer == str(right_answer):
-            print('Correct!')
-            count += 1
-        else:
-            print(f"'{user_answer}' is wrong answer ;(.' \
-                Correct answer was '{right_answer}'")
-            print(f"Let's try again, {user_name}!")
-            break
-        print(f"Congratulations, {user_name}!")
+def game_task():
+    number1, number2 = randint(1, 10), randint(1, 10)
+    operator, sign = choice(operations)
+    right_answer = str(operator(number1, number2))
+    question = f'{number1} {sign} {number2}'
+    return question, right_answer
