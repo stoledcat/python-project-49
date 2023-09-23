421d72ccb0aef3ce784e2ce2ce16370ce694f66c
stoledcat <jetnavo@yandex.ru>
2023-09-23 20:37:28 +0300

    refactoring

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-from brain_games.games.even import is_even
+from brain_games.engine import repeated_code
+from brain_games.games import even
 
 
 def main():
-    is_even()
-
-
-if __name__ == '__main__':
-    main()
+    repeated_code(even)
