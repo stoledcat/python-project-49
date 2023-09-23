7d59fea9680d30a9739a7dd3de46ac47f63cb5d3
stoledcat <jetnavo@yandex.ru>
2023-09-23 18:49:39 +0300

    add engine

---
+++ brain_games/engine.py
@@ -0,0 +1,22 @@
+import prompt
+
+
+def repeated_code(task):
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print(task.description)
+    win_count = 0
+    rounds = 3
+    while win_count < rounds:
+        question, right_answer = task.game_task()
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+        if right_answer == user_answer:
+            print('Correct!')
+            win_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(.' \
+Correct answer was '{right_answer}'")
+            break
+        print(f'Congratulations, {user_name}!')
