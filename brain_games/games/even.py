ca9f7b0d6e81d9b375cb6505545b61e39545ad19
stoledcat <jetnavo@yandex.ru>
2023-09-23 20:38:18 +0300

    first refactoring

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,21 +1,14 @@
 import random
-import prompt
-from brain_games.cli import welcome_user
+# import prompt
 
 
-def is_even():
-    user_name = welcome_user()
-    correct_answer = ['0yes', '1no']  # правильные ответы
-    count = 0  # счетчик
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    for i in range(3):
-        number = random.randint(1, 100)
-        print('Question:', number)  # выводится сгенерированное число
-        answer = prompt.string('Your answer: ')  # ввод ответа yes/no
-        if str(number % 2) + answer in correct_answer:  # сверяемся с ответами
-            print('Correct!')
-            count += 1
-        else:
-            print("Wrong answer. Let's try again,", user_name + "!")
-            break
-        print("Congratulations,", user_name + "!")
+description = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def game_task():
+    pattern = ['0yes', '1no']
+    number = random.randint(1, 100)
+    # if str(number % 2) + user_answer in pattern:
+    question = number
+    right_answer = 'yes' if str(number % 2) + user_answer in pattern else 'no'
+    return question, right_answer
