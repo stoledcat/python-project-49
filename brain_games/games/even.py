b0802288c2a7a460536d6036a65c5770431de647
stoledcat <jetnavo@yandex.ru>
2023-09-23 22:12:05 +0300

    minor fix

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,14 +1,11 @@
 import random
-# import prompt
 
 
 description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def game_task():
-    pattern = ['0yes', '1no']
     number = random.randint(1, 100)
-    # if str(number % 2) + user_answer in pattern:
     question = number
-    right_answer = 'yes' if str(number % 2) + user_answer in pattern else 'no'
+    right_answer = 'yes' if (number % 2) == 0 else 'no'
     return question, right_answer
