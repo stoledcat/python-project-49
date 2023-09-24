c4bd8e9daf305cf25e07fe8c4e1bfbdd60d4ce4c
stoledcat <jetnavo@yandex.ru>
2023-09-24 23:06:56 +0300

    refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,19 +6,13 @@ description = 'What number is missing in the progression?'
 
 def game_task():
     # генерация трех чисел указана с 2, чтобы в итоговом ряду было 5+ элементов
-    start_number, stop_number, step = (random.randint(2, 10) for i in range(3))
+    start_number, stop_number, step = (random.randint(2, 30) for i in range(3))
     # гарантированное увеличение STOP для range
-    stop_number = (start_number + stop_number + step)
-    # print('Question: ', end='')
-    for i in range(start_number, stop_number):  # генерация числового ряда
-        print(start_number, end=' ')
-        # сравнение счетчика с рандомным числом
-        # для замены этого числа в ряду на '..'
-        if i == stop_number - step:
-            print('..', end=' ')
-            # сдвиг следующего числа в ряду на шаг
-            start_number += step
-            right_answer = start_number
-        start_number += step
-        question = start_number
+    stop_number = (start_number + stop_number * step)
+    # создние арифметической прогрессии
+    progression = list(range(start_number, stop_number, step))
+    right_answer = random.choice(progression)
+    question = '' + ' '.join(
+        '..' if num == right_answer else str(num) for num in progression
+    )
     return question, str(right_answer)
