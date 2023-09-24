5cd0b407616f273b8d5436a39205e7ef003f18c5
stoledcat <jetnavo@yandex.ru>
2023-09-24 23:14:10 +0300

    изменил минимальное количество чисел в прогресии

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,10 +5,9 @@ description = 'What number is missing in the progression?'
 
 
 def game_task():
-    # генерация трех чисел указана с 2, чтобы в итоговом ряду было 5+ элементов
     start_number, stop_number, step = (random.randint(2, 30) for i in range(3))
     # гарантированное увеличение STOP для range
-    stop_number = (start_number + stop_number * step)
+    stop_number = (start_number + stop_number + step) * 5
     # создние арифметической прогрессии
     progression = list(range(start_number, stop_number, step))
     right_answer = random.choice(progression)
