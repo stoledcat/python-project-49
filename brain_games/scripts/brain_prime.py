f667522b5ced08578de2c2f2a9b6aa26988d1945
stoledcat <140549870+stoledcat@users.noreply.github.com>
2023-09-29 08:17:25 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 from brain_games.engine import repeated_code
 from brain_games.games import prime
 
 
 def main():
     repeated_code(prime)
+
+
+if __name__ == '__main__':
+    main()
