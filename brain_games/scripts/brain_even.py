fa3894131f6121bb1318016305e9d73a280e2190
stoledcat <140549870+stoledcat@users.noreply.github.com>
2023-09-29 08:15:35 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+
 from brain_games.engine import repeated_code
 from brain_games.games import even
 
 
 def main():
     repeated_code(even)
+
+
+if __name__ == '__main__':
+    main()
