d9da257f1c7615d6112bd4b0fc2097f2883abd3f
stoledcat <140549870+stoledcat@users.noreply.github.com>
2023-09-29 08:17:49 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 from brain_games.engine import repeated_code
 from brain_games.games import progression
 
 
 def main():
     repeated_code(progression)
+
+
+if __name__ == '__main__':
+    main()
