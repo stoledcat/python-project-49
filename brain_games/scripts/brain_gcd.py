5c91ed4d447010dc59124dde07784f0135f0e9e4
stoledcat <140549870+stoledcat@users.noreply.github.com>
2023-09-29 08:16:54 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 from brain_games.engine import repeated_code
 from brain_games.games import gcd
 
 
 def main():
     repeated_code(gcd)
+
+
+if __name__ == '__main__':
+    main()
