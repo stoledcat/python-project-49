8eb4a392fe0b0e4b0b44289b7fe86d736b92eddc
stoledcat <jetnavo@yandex.ru>
2023-09-30 18:24:07 +0300

    refactoring

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,6 @@
 import prompt
 
 ROUNDS = 3
-WIN_COUNT = 0
 
 
 def repeated_code(task):
@@ -9,17 +8,18 @@ def repeated_code(task):
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     print(task.DESCRIPTION)
-    while WIN_COUNT < ROUNDS:
+    win_count = 0
+    while win_count < ROUNDS:
         question, right_answer = task.game_task()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
         if right_answer == user_answer:
             print('Correct!')
-            WIN_COUNT += 1
+            win_count += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(.' \
 Correct answer was '{right_answer}'")
             print(f"Let's try again, {user_name}!")
             break
-    if WIN_COUNT == 3:
+    if win_count == 3:
         print(f'Congratulations, {user_name}!')
