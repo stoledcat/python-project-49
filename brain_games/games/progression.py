4d3cea7805385769b561967c4131c9c6efdf312a
stoledcat <140549870+stoledcat@users.noreply.github.com>
2023-10-02 23:27:40 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,10 +4,15 @@ import random
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def start_function():
+def generate_question():
     start_number, stop_number, step = (random.randint(2, 30) for i in range(3))
     stop_number = (start_number + stop_number + step) * 4
     question = list(range(start_number, stop_number, step))
+    return question
+
+
+def start_function():
+    question = generate_question()
     len_question = len(question)
     index_answer = random.choice(range(0, len_question))
     right_answer = question[index_answer]
