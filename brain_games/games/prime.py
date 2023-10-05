7dbf887bcb3605bbec3de27eff3dd818d36d5ece
stoledcat <jetnavo@yandex.ru>
2023-10-05 16:22:59 +0300

    refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,16 +4,20 @@ import random
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def generate_random_num():
-    random_num = random.randint(2, 101)
-    return random_num
-
-
-def start_function():
-    random_num = generate_random_num()
+def check_prime(number):
     divider = 2
-    while divider < random_num:
-        if random_num % divider == 0:
+    while divider < number:
+        if number % divider == 0:
             return False
         divider += 1
     return True
+
+
+def start_function():
+    random_num = random.randint(2, 10)
+    question = random_num
+    if check_prime(random_num):
+        right_answer = "yes"
+    else:
+        right_answer = "no"
+    return question, right_answer
